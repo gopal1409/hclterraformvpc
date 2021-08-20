924537d3abd1d84b6e831f5a61a3957bacc780c1
Gopal <Gopal1409@gmail.com>
2021-08-20 02:38:36 -0700

    terraform with vpc module

---
+++ vpc.tf
@@ -0,0 +1,19 @@
+#create vpc terraform module
+module "vpc" {
+    source =  "terraform-aws-modules/vpc/aws"
+    version = "3.6.0"
+
+    name = "vpc-dev-gopal" #change it to your name
+    azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+    private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+    public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+    enable_nat_gateway = true
+    single_nat_gateway = true
+
+    #vpc dns parameters
+    enable_dns_support = true 
+    enable_dns_hostnames= true
+  
+
+}
