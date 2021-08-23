5a21bfc8e67044189dcc856990fae3b5ffdc65b6
Gopal <Gopal1409@gmail.com>
2021-08-23 03:20:07 -0700

    elb

--- ELB-load-balancer.tf
+++ ELB-load-balancer.tf
@@ -27,7 +27,7 @@ module "elb" {
       target = "HTTP:80/"
       interval = 30
       healthy_threshold = 2
-      unhealth_threshold = 2
+      unhealthy_threshold = 2
       timeout = 5
   }
   #Behind the load balancer we need to attach instance. 
