2f4e9591119e3223789258631d66e00f6020eb6a
Gopal <Gopal1409@gmail.com>
2021-08-23 03:17:07 -0700

    elb

--- ec2instance-private.tf
+++ ec2instance-private.tf
@@ -6,10 +6,7 @@ module "ec2_private" {
   ami = data.aws_ami.amznlinux2.id 
   instance_type = var.instance_type
   #vpc_security_group_ids = [module.private_sg]
-  subnet_id = [
-      module.vpc.private_subnets[0],
-      module.vpc.private_subnets[1]
-  ]
+  subnet_id = [module.vpc.private_subnets[0],module.vpc.private_subnets[1]]
   instance_count  = var.private_instance_count
   user_data = file("${path.module}/app1-install.sh")
   tags = local.common_tags
